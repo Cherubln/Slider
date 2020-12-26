3ea5eb12735a111e554c877eed0b04a0d70acd3f
Cherubin <cherubinkubwayo@gmail.com>
2020-12-26 13:21:00 +0200

    edit document title

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect } from 'react';
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+import data from './data';
 function App() {
+  const [people, setPeople] = useState(data);
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    index < 0
+      ? setIndex(people.length - 1)
+      : index > people.length - 1
+      ? setIndex(0)
+      : setIndex(index);
+  }, [index, people]);
+
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setIndex(index + 1);
+    }, 3000);
+    return () => clearInterval(slider);
+  }, [index]);
+  const prevPerson = () => {
+    setIndex((state) => state - 1);
+  };
+  const nextPerson = () => {
+    setIndex((state) => state + 1);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className='section'>
+      <div className='title'>
+        <h2>
+          <span>/</span> Reviews
+        </h2>
+        <div className='section-center'>
+          {people.map((person, personIndex) => {
+            const { id, quote, image, name, title } = person;
+            // more staff coming
+            let position = 'nextSlide';
+            if (personIndex === index) position = 'activeSlide';
+            if (personIndex === (index - 1 + people.length) % people.length)
+              position = 'lastSlide';
+            return (
+              <article key={id} className={position}>
+                <img src={image} alt={name} className='person-img' />
+                <h4>{name}</h4>
+                <p className='title'>{title}</p>
+                <p className='text'>{quote}</p>
+                <FaQuoteRight className='icon' />
+              </article>
+            );
+          })}
+          <button className='prev' onClick={prevPerson}>
+            <FaChevronLeft />
+          </button>
+          <button className='next' onClick={nextPerson}>
+            <FaChevronRight />
+          </button>
+        </div>
+      </div>
+    </section>
   );
 }
 
